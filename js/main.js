d1d2e8eaf078333b493e52044d7455f196ccdf23
slonov-dev <83721305+slonov-dev@users.noreply.github.com>
2021-05-09 08:37:05 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -875,12 +875,6 @@ function parallaxInfrastructureInit() {
 } //del animate class
 
 
-var pageInf = document.querySelector('.page-infrastructure-animation');
-
-if (pageInf) {
-  setTimeout(parallaxInfrastructureInit, 500);
-}
-
 var infrastructureAnimation_1 = document.querySelector(".infrastructure-animation_1");
 
 if (infrastructureAnimation_1) {
@@ -910,6 +904,7 @@ var infrastructureAnimation_4 = document.querySelector(".infrastructure-animatio
 if (infrastructureAnimation_4) {
   infrastructureAnimation_4.addEventListener('animationend', function () {
     infrastructureAnimation_4.classList.remove('animate__animated', 'animate__zoomInUp');
+    setTimeout(parallaxInfrastructureInit, 500);
   });
 }
 
