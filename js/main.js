574e9e46f92c68825c36030d2d35b1cbcf14c7ba
slonov-dev <83721305+slonov-dev@users.noreply.github.com>
2021-05-12 14:09:24 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -368,19 +368,21 @@ $('.card-group__header').click(function () {
     $(this).next().show('slow');
   }
 });
-$('.header__link_submenu').click(function () {
+$('.submenu__arrow-wrap').click(function () {
+  console.log('click');
   $(this).find('.submenu__arrow-wrap svg').css({
     'transform': 'rotate(0deg)'
   });
 
-  if ($(this).next().is(":visible")) {
-    $(this).next().hide('slow');
+  if ($('.submenu').is(":visible")) {
+    $('.submenu').hide('slow');
     $('.submenu__arrow-wrap svg').css({
       'transform': ' rotate(180deg)'
     });
+    console.log('click1');
   } else {
-    $('.submenu:visible').hide('slow');
-    $(this).next().show('slow');
+    $('.submenu').show('slow');
+    console.log('click2');
   }
 });
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
